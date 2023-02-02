6fa194c959ac32b4dd2569e217c3ce243d9605c4
N0-Lifer <88077798+MQ-NickW@users.noreply.github.com>
2023-02-02 22:43:45 +1100

    Add project files.

---
+++ MyTicTacToe/App.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyTicTacToe
+{
+    public class App
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ MyTicTacToe/Board.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTicTacToe
+{
+    internal class Board
+    {
+        Cell[,] cell;
+        public Board() {
+            cell = new Cell[3,3];
+        }
+    }
+}
